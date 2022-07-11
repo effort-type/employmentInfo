665af9fdc6946ba4cef6d100bb50ea9e73d73546
Minsu <jminsu084@gmail.com>
2022-07-11 20:48:00 +0900

    UPDATE : 200페이지 한정으로 되어있던 것을 동적 페이지 전체로 범위 확대 완료

--- jumpit.py
+++ jumpit.py
@@ -15,35 +15,27 @@ headers = {
 
 # 동적 페이지 숫자 변경해주는 함수
 def change_url_page(page):
-    if page > PAGE:
-        return "400" # 정보가 없는 페이지라는 것을 표시하기 위해 400을 반환
-    else:
-        global dynamic_page # 동적 페이지 번호 전역 변수로 선언
-        dynamic_page = page + 1
-        url_temp = "https://api.jumpit.co.kr/api/positions?page=" + str(dynamic_page) + "&sort=reg_dt&highlight=false"
-        return url_temp
+    global dynamic_page # 동적 페이지 번호 전역 변수로 선언
 
+    dynamic_page = page + 1
+    url_temp = "https://api.jumpit.co.kr/api/positions?page=" + str(dynamic_page) + "&sort=reg_dt&highlight=false"
+    return url_temp
 
-for page in range(PAGE):
+# 기술 json이 공백이 나올 때까지 루프
+while True:
     result = requests.get(url, headers=headers)
     result.raise_for_status()  # 정상적으로 접속이 되었는지 확인
     result.encoding = "utf8"
 
-    stock_data = json.loads(result.text) # json 형태의 데이터 저장
+    stock_data = json.loads(result.text)  # json 형태의 데이터 저장
+
+    # 기술 json이 빈칸인 경우
+    if len(stock_data['result']['positions']) == 0:
+        break
 
     for data in stock_data['result']['positions']:
         for skill in data['techStacks']:
             print(skill)
 
-    print("----- 동적 " + str(page + 1) + "페이지 끝 -----")
+    print("----- 동적 " + str(dynamic_page + 1) + "페이지 끝 -----")
     url = change_url_page(dynamic_page)
-
-    if url == "400":
-        print("error")
-        break
-
-
-# with open("jumpit.json", "w", encoding="utf8") as f:
-#     f.write(result.text)
-#
-# soup = BeautifulSoup(result.text, "lxml")  # lxml 라이브러리 설치 후 사용, lxml은 parser
